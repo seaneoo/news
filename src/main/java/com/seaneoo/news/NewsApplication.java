412ad259ff2330fc01583130fcc84fa74ff2b0cd
Sean O'Connor <so@seano.dev>
2024-07-23 15:36:50 -0600

    Create User entity

--- src/main/java/com/seaneoo/news/NewsApplication.java
+++ src/main/java/com/seaneoo/news/NewsApplication.java
@@ -2,8 +2,10 @@ package com.seaneoo.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NewsApplication {
 
 	public static void main(String[] args) {
