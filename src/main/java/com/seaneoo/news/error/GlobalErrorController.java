13a926ff0009e79638b2e7c55d50b4430b314662
Sean O'Connor <so@seano.dev>
2024-07-30 16:48:42 -0600

    Fix GlobalErrorController not displaying the correct information

--- src/main/java/com/seaneoo/news/error/GlobalErrorController.java
+++ src/main/java/com/seaneoo/news/error/GlobalErrorController.java
@@ -7,17 +7,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import static jakarta.servlet.RequestDispatcher.*;
 
 @RestController
 public class GlobalErrorController implements ErrorController {
 
 	@RequestMapping("/error")
 	public ResponseEntity<ExceptionResponse> handleErrorMapping(HttpServletRequest req, HttpServletResponse res) {
-		var httpStatus = HttpStatus.valueOf(res.getStatus());
+		var statusCode = Integer.parseInt(req.getAttribute(ERROR_STATUS_CODE).toString());
+		var errorMessage = "";
+
+		var throwable = (Throwable) req.getAttribute(ERROR_EXCEPTION);
+		if (throwable != null) {
+			if (throwable instanceof ResponseStatusException) {
+				errorMessage = ((ResponseStatusException) throwable).getReason();
+				statusCode = ((ResponseStatusException) throwable).getStatusCode().value();
+			} else {
+				errorMessage = throwable.getMessage();
+			}
+		}
+
+		var httpStatus = HttpStatus.valueOf(statusCode);
+		var requestUri = req.getAttribute(ERROR_REQUEST_URI);
+
 		var response = ExceptionResponse.builder()
 			.statusCode(httpStatus.value())
 			.statusReason(httpStatus.getReasonPhrase())
-			.path(req.getRequestURI())
+			.path(requestUri.toString())
+			.message(errorMessage)
 			.build();
 		return ResponseEntity.status(httpStatus).body(response);
 	}
